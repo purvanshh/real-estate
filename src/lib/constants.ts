66e6d1f9ac4be3d76b683b6c036a5b28285cac96
Purvansh <purvansh.23bcs10169@ms.sst.scaler.com>
2025-12-28 22:59:40 +0530

    chore: Update architecture image URL in constants.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,5 +5,5 @@ export const IMAGES = {
     project3: "https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-4.0.3&auto=format&fit=crop&w=2860&q=80",
     interior1: "https://images.unsplash.com/photo-1507652313519-d4e9174996dd?ixlib=rb-4.0.3&auto=format&fit=crop&w=2850&q=80",
     interior2: "https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-4.0.3&auto=format&fit=crop&w=2850&q=80",
-    architecture: "https://images.unsplash.com/photo-1487956382158-bb926046304a?ixlib=rb-4.0.3&auto=format&fit=crop&w=2850&q=80",
+    architecture: "https://images.unsplash.com/photo-1553027257-7f791c15f20d?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 }
